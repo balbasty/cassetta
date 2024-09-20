baf268b01780bdab35a39561eefde1821c7b9d0c
EtienneChollet <etiennechollet2626@gmail.com>
2024-09-19 20:09:33 -0400

    [FEAT] Add regression loss module

---
+++ cassetta/losses/regression.py
@@ -0,0 +1,19 @@
+__all__ = [
+    'LoadableMSE'
+]
+
+from torch.nn import MSELoss
+from cassetta.io.modules import LoadableMixin
+
+
+class LoadableMSE(LoadableMixin, MSELoss):
+    """
+    A loadable variant of PyTorch's Adam optimizer
+    [`torch.nn`][`torch.nn.MSELoss`]
+    """
+    @LoadableMixin.save_args
+    def __init__(self):
+        """
+        Initialize the LoadableMSE loss function.
+        """
+        super().__init__()
