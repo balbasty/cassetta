a710cc5559b45a62eb257126390eac0a41127c03
EtienneChollet <etiennechollet2626@gmail.com>
2024-10-04 11:03:32 -0400

    [FIX] Datasets: Use `import_submodules()`

--- cassetta/datasets/__init__.py
+++ cassetta/datasets/__init__.py
@@ -1,2 +1,15 @@
-from . import supervised
-from .supervised import *
+"""
+## Overview
+
+Datasets organize and allow for dynamic preprocessing of data.
+
+Modules
+-------
+supervised
+    Datasets for supervised learning tasks with paired data (input-output)
+"""
+from cassetta.core.utils import import_submodules
+
+import_submodules([
+    'supervised',
+], __name__, True)
