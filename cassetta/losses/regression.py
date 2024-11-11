86f1742427ea02f361230a74769b4e4a9b66b81b
Etienne Chollet <etiennepchollet@gmail.com>
2024-11-11 09:06:21 -0500

    [REFACTOR] Remove @LoadableMixin.save_args from LoadableMSE.

--- cassetta/losses/regression.py
+++ cassetta/losses/regression.py
@@ -11,7 +11,6 @@ class LoadableMSE(LoadableMixin, MSELoss):
     A loadable variant of PyTorch's MSE loss.
     [`torch.nn`][`torch.nn.MSELoss`]
     """
-    @LoadableMixin.save_args
     def __init__(self):
         """
         Initialize the LoadableMSE loss function.
